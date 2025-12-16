08ee6c6b6e36910969d45d6d6c442bd59b6710fa
yulia <tus10082006@gmail.com>
2025-12-16 17:20:47 +0300

    feat(models): add Goal model for Yulia

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Goal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    title = models.CharField(max_length=200, verbose_name="Ваша цель")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Цель"
+        verbose_name_plural = "Цели"
